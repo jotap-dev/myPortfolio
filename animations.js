5d135f871996c5e6de7c89c10440432e7a0e02ff
João Pedro <JoaoPedro.stack@gmail.com>
2023-05-23 16:43:09 -0300

    responsive is added to the project

--- animations.js
+++ animations.js
@@ -1,9 +1,14 @@
+window.alert('em desenvolvimento!, aproveite!')
+
 function switchTheme(){
     const themeButton = document.querySelector('.themeSwitch')
     const selectedTheme = document.querySelector('.selectTheme')
     const pageHeader = document.querySelector('.pageHeader')
     const techs = document.querySelector('.center')
-
+    const meInfo = document.querySelector('.infoContainer')
+    const stats = document.querySelector('.stats')
+    const card = document.querySelectorAll('#cardContainer a')
+    const seeAll = document.querySelector('.seeAll')
     const light = 'translateX(100%)'
     const dark = 'translateX(0%)'
 
@@ -17,6 +22,12 @@ function switchTheme(){
         pageHeader.borderColor = 'black'
         techs.style.backgroundColor = 'white'
         techs.style.color = 'black'
+        meInfo.style.backgroundColor = 'white'
+        stats.style.color = 'black'
+        card[0].style.backgroundColor = 'white'       
+        card[1].style.backgroundColor = 'white'  
+        card[2].style.backgroundColor = 'white'
+        seeAll.style.color = 'black'
     }else{
         selectedTheme.style.transform = dark
         selectedTheme.style.backgroundColor = 'white'
@@ -27,5 +38,15 @@ function switchTheme(){
         pageHeader.borderColor = 'white'
         techs.style.backgroundColor = 'rgb(60,60,60)'
         techs.style.color = 'white'
+        meInfo.style.backgroundColor = 'rgb(60,60,60)'
+        stats.style.color = 'white'
+        card[0].style.backgroundColor = 'rgb(60,60,60)'
+        card[1].style.backgroundColor = 'rgb(60,60,60)'
+        card[2].style.backgroundColor = 'rgb(60,60,60)'
+        seeAll.style.color = 'white'
     }
+}
+
+function backToTop(){
+    window.scrollTo(0,0)
 }
